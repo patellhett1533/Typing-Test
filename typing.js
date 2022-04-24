df36af77de62f12076565ba483579e32c6668378
Patel Het <103815535+patellhett1533@users.noreply.github.com>
2022-04-24 18:27:38 +0530

    Add files via upload

---
+++ typing.js
@@ -0,0 +1,132 @@
+const paragraph = [
+    "Sunset is the time of day when our sky meets the outer space solar winds. There are blue, pink, and purple swirls, spinning and twisting, like clouds of balloons caught in a whirlwind. The sun moves slowly to hide behind the line of horizon, while the moon races to take its place in prominence atop the night sky. People slow to a crawl, entranced, fully forgetting the deeds that must still be done. There is a coolness, a calmness, when the sun does set.",
+    "On July 16, 1969, the Apollo spacecraft launched from the Kennedy Space Center in Florida. Its mission was to go where no human being had gone before—the moon! The crew consisted of Neil Armstrong, Michael Collins, and Buzz Aldrin. The spacecraft landed on the moon in the Sea of Tranquility, a basaltic flood plain, on July 20, 1969. The moonwalk took place the following day. On July 21, 1969, at precisely 10:56 EDT, Commander Neil Armstrong emerged from the surface of the moon.",
+    "Here is the perfect system for cleaning your room. First, move all of the items that do not have a proper place to the center of the room. Get rid of at least five things that you have not used within the last year. Take out all of the trash, and place all of the dirty dishes in the kitchen sink. Now find a location for each of the items you had placed in the center of the room. For any remaining items, see if you can squeeze them in under your bed or stuff them into the back of your closet. See, that was easy!",
+    "Oceans and lakes have much in common, but they are also quite different. Both are bodies of water, but oceans are very large bodies of salt water, while lakes are much smaller bodies of fresh water. Lakes are usually surrounded by land, while oceans are what surround continents. Both have plants and animals living in them. The ocean is home to the largest animals on the planet, whereas lakes support much smaller forms of life. When it is time for a vacation, both will make a great place to visit and enjoy.",
+    "The Blue Whales just played their first baseball game of the new season; I believe there is much to be excited about. Although they lost, it was against an excellent team that had won the championship last year. The Blue Whales fell behind early but showed excellent teamwork and came back to tie the game. The team had 15 hits and scored 8 runs. That's excellent! Unfortunately, they had 5 fielding errors, which kept the other team in the lead the entire game.",
+    "The school fair is right around the corner, and tickets have just gone on sale. We are selling a limited number of tickets at a discount, so move fast and get yours while they are still available. This is going to be an event you will not want to miss! First off, the school fair is a great value when compared with other forms of entertainment. Also, your ticket purchase will help our school, and when you help the school, it helps the entire community. But that's not all!",
+    "Last week we installed a kitty door so that our cat could come and go as she pleases. Unfortunately, we ran into a problem. Our cat was afraid to use the kitty door. We tried pushing her through, and that caused her to be even more afraid. The kitty door was dark, and she couldn't see what was on the other side. The first step we took in solving this problem was taping the kitty door open. After a couple of days, she was confidently coming and going through the open door.",
+    "This began as one paragraph, but needed two—one for the problem and one for the solution. Also, notice that the second paragraph is a process paragraph. It would be very easy to add an introduction and conclusion to these two paragraphs and have a complete essay! People often install a kitty door, only to discover that they have a problem. The problem is their cat will not use the kitty door. There are several common reasons why cats was not use kitty doors.",
+    "First, they may not understand how a kitty door works. They may not understand that it is a little doorway just for them. Second, many kitty doors are dark, and cats cannot see to the other side. As such, they can't be sure of what is on the other side of the door, so they won't take the risk. One last reason cats won't use kitty doors is because some cats don't like the feeling of pushing through the door and having the door drag across their back.",
+    "The first step in solving the problem is to prop the door open with tape. This means your cat will now be able to see through to the other side; your cat will likely begin using the kitty door immediately. Once your cat has gotten used to using the kitty door, remove the tape. Sometimes cats will continue to use the kitty door without any more prompting. If this does not happen, you will want to use food to bribe your cat.",
+    "Your social networks and your location within them shape the kinds and amount of information that you have access to. Information is distinct from data, in that makes some kind of generalization about a person, thing, or population. Defensible generalizations about society can be either probabilities (i.e., statistics) or patterns (often from qualitative analysis). Such probabilities and patterns can be temporal, spatial, or simultaneous.",
+    "The experiment seemed straightforward and there were plenty of scientists willing to try it. Many did. It was wonderful to have a simple laboratory experiment on fusion to try after the decades of embarrassing attempts to control hot fusion. This effort required multi-billion dollar machines whose every success seemed to be capped with an unanticipated failure. 'Cold fusion' seemed to provide, as Martin Fleischmann said during the course of that famous Utah press conference,the route of little science.",
+    "As a young industry that champions innovation and rates its practitioners based on their ability to apprehend (sorry, 'grok') the continual emergence of new technologies, frameworks, protocols and data models, we are not particularly familiar with tradition. However, the practice of arranging type for optimal pleasure and comfort is a centuries-old discipline. As long ago as 1927, the noted typographer Jan Tschichold spoke of the typesetting 'methods and rules upon which it is impossible to improve'.",
+    "Suspendisse rhoncus consectetur lectus, vitae gravida felis dictum ac. Suspendisse potenti. Praesent eu libero at nisl congue feugiat. Mauris arcu nisl, feugiat ut auctor nec, aliquet vel odio. Praesent aliquet, mi eget dignissim aliquam, lorem nunc venenatis libero, vitae vulputate ante mauris vitae sem. Donec pharetra purus euismod nunc dignissim ullamcorper. Donec pulvinar pulvinar ornare. Mauris a vestibulum risus. Nullam at ligula volutpat, tempor felis fermentum, ullamcorper purus.",
+    "Sed placerat ante mollis felis pellentesque, quis hendrerit elit porttitor. Integer commodo mauris blandit magna euismod, eget suscipit purus vehicula. Mauris ut dolor lacus. Sed eu diam vulputate, vehicula ante in, lobortis nibh. Praesent tortor neque, congue et nisl id, egestas hendrerit orci. Nunc posuere ante vel eros congue, nec dapibus augue posuere. Ut in interdum turpis. Morbi eget orci sit amet sapien mattis tincidunt sed id quam. Curabitur id nibh et nunc sagittis auctor nec fringilla enim.",
+    "Ut sagittis rutrum purus, et congue odio dapibus sit amet. Vestibulum venenatis odio vel cursus rutrum. Vestibulum luctus sapien arcu, at iaculis nunc fringilla ut. Phasellus in ipsum suscipit, tempor ante mattis, sodales lectus. Ut condimentum diam est, eget varius lorem rhoncus et. Nam id enim vehicula nulla sodales consectetur. Nunc ultrices sem et eleifend facilisis. Sed id mi tempor, mollis velit sit amet, rutrum ligula. Suspendisse metus leo, euismod in sollicitudin tempor, consequat in sapien.",
+    "Integer porttitor lacus odio. Aliquam vitae leo tincidunt urna dapibus vehicula at elementum ligula. Donec viverra erat vel magna laoreet, nec convallis orci pellentesque. Aenean turpis leo, consectetur fermentum libero et, eleifend gravida orci. Vestibulum sodales velit erat, vel cursus enim efficitur eu. Nullam quis lorem at lectus ultricies bibendum quis vitae nunc. Quisque in venenatis purus. Suspendisse potenti. Sed pretium sit amet risus id lobortis. Curabitur aliquam feugiat viverra.",
+    "Donec egestas, augue nec sagittis lacinia, diam risus cursus nibh, ac venenatis diam massa eget odio. Suspendisse tempor, massa eget consectetur gravida, diam magna cursus arcu, in posuere mi elit nec felis. Pellentesque eget gravida nisl, ac imperdiet urna. Nam a velit non arcu convallis imperdiet. Proin condimentum gravida blandit. Nunc vestibulum dignissim maximus. Praesent id vestibulum lorem. Suspendisse at leo vitae est scelerisque accumsan. Vivamus quis interdum justo.",
+    "Proin in hendrerit turpis. Donec ut sem ac purus ultricies imperdiet. Integer rhoncus erat sed tortor pharetra, in aliquam lectus maximus. Maecenas tortor purus, ultricies blandit maximus sed, vulputate a magna. Nunc hendrerit velit id enim vehicula pretium. Aliquam tempus accumsan ante, et porta massa malesuada sed. Etiam vel odio sem. Duis at neque et ante suscipit cursus vel in felis. Ut iaculis tincidunt malesuada. Nam sit amet posuere leo, at semper leo. Nunc ac lobortis leo.",
+    "Nam ornare aliquam luctus. Etiam vel lobortis felis, in dictum turpis. Aenean elit lorem, congue quis nisl a, vehicula posuere leo. Donec quis mollis justo. Praesent ac eros eleifend, ornare erat et, placerat nulla. Praesent viverra feugiat sapien, eget accumsan quam vulputate id. Quisque varius, urna sed ullamcorper ultrices, justo odio egestas ligula, ut tincidunt nunc urna vel ante. Fusce neque ante, vestibulum ac imperdiet eget, mollis commodo massa. Vivamus condimentum nisi nec finibus rutrum.",
+    "Vestibulum vestibulum aliquam lacus, et ornare nulla pharetra in. Donec ut fringilla orci. Vestibulum placerat, velit sit amet ullamcorper pulvinar, odio ex aliquam ante, nec molestie neque est sit amet arcu. Curabitur consectetur, nunc eu rhoncus efficitur, libero nunc imperdiet tellus, at congue augue mauris quis risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras posuere tortor et tempus congue. Suspendisse in justo sit amet libero faucibus iaculis at ut mi.",
+    "Praesent pellentesque dapibus dui, sit amet placerat odio accumsan et. Phasellus feugiat augue a fermentum blandit. Donec porttitor porta posuere. Cras ac pulvinar leo. Integer lobortis felis vel ullamcorper placerat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer ut bibendum erat. Praesent vel velit porttitor, commodo diam a, suscipit velit. Proin pulvinar eget mi tristique mattis.",
+    "In neque nisi, volutpat non luctus sit amet, sollicitudin quis lectus. Proin nisl felis, ornare congue tellus at, aliquet venenatis eros. Praesent id ipsum sollicitudin, porta lacus non, condimentum ipsum. Praesent elementum orci sit amet nibh ornare ultrices. Maecenas lorem lacus, molestie vel pellentesque eu, auctor eu sapien. Proin rutrum auctor mi eu vestibulum. Maecenas nec ullamcorper nunc. Duis eu dolor et tortor elementum convallis. Vivamus sit amet purus nec leo tempus egestas. Aliquam nisl lacus.",
+    "Nullam et tempus risus. Pellentesque commodo, ex ac facilisis convallis, odio risus venenatis risus, at sodales augue velit vel lorem. Suspendisse ultricies, tellus vitae venenatis ullamcorper, nulla ante condimentum felis, sit amet aliquet erat tortor ac nisi. Donec consequat posuere blandit. Sed enim eros, aliquet hendrerit augue vel, condimentum scelerisque mauris. Vivamus a nibh felis. Cras non diam dignissim, tempus nisl eget, cursus diam. Nulla a porta felis, id tincidunt nulla.",
+    "Duis venenatis eleifend justo. Mauris sed elit vulputate, aliquam leo at, mollis tellus. Duis auctor, sapien non ultrices efficitur, ex purus ultrices tellus, in sollicitudin leo massa ut tellus. Donec vestibulum mauris tellus, vitae consectetur massa egestas nec. Nulla vel tempor ante. Cras in nisi at magna sodales auctor. Sed nec libero at ex feugiat sollicitudin sit amet sit amet nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam condimentum placerat enim ac mollis.",
+    "Vivamus id orci in augue cursus sodales sit amet sit amet justo. Vivamus consectetur erat id eros auctor, non fringilla purus vulputate. Maecenas convallis vehicula risus at tristique. Fusce iaculis neque tincidunt arcu dapibus bibendum ac tristique metus. Vivamus ac augue dictum, sagittis nunc sit amet, viverra nunc. Nam nec bibendum mauris, vel dignissim tortor. Nam quam nibh, pretium ac consectetur nec, maximus ac orci. Orci varius natoque penatibus et magnis dis parturient montes.",
+    "Sed lobortis sapien et tincidunt rutrum. Sed sit amet augue eu dui laoreet aliquet at sed magna. Maecenas semper, diam at elementum posuere, velit ipsum tempus ligula, sit amet dignissim tellus odio sit amet purus. Nunc et auctor orci. Vivamus hendrerit ornare sapien. Sed tincidunt scelerisque lobortis. Mauris est massa, maximus quis est sit amet, sodales rhoncus arcu. Donec at nulla ac sem semper cursus dignissim id elit. Cras lobortis purus lacus, non aliquet augue luctus sed. Praesent malesuada eu ex quis porta."
+];
+
+const typingText = document.getElementById('text');
+const setParagraph = document.getElementById('setParagraph');
+const textField = document.getElementById('textField');
+const mistakeTag = document.getElementById('mistakeTag');
+const timeTag = document.getElementById('timeTag');
+const wpmTag = document.getElementById('wpmTag');
+const reset = document.getElementById('reset');
+
+let timer,
+    maxTime = 60;
+let timeleft = maxTime;
+
+let charIndex = 0;
+let mistake = 0;
+let isTyping = 0;
+
+function randomParagraph() {
+    // logic of fetching random paragraph as a character
+    let randomIndex = Math.floor(Math.random() * paragraph.length);
+    setParagraph.innerHTML = "";
+    paragraph[randomIndex].split("").forEach(span => {
+        let spanTag = `<span>${span}</span>`;
+        setParagraph.innerHTML += spanTag;
+    })
+    // click or press any key to start typing
+    document.addEventListener('keydown', () => textField.focus());
+    typingText.addEventListener('click', () => textField.focus());
+}
+
+function initField() {
+    const characters = typingText.querySelectorAll("span");
+    let typeChar = textField.value.split("")[charIndex];
+
+    if (charIndex < characters.length - 1 && timeleft > 0) {
+        // start time one time when click first character
+        if (!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+
+        // if typed character is nuli or backspace key then 
+        if (typeChar == null) {
+            charIndex--;
+            // mistake decreament if the charecter is wrong
+            if (characters[charIndex].classList.contains("no")) {
+                mistake--;
+            }
+            // if press backspace key then earse the charecter
+            characters[charIndex].classList.remove("yes", "no");
+        }
+        else {
+            // if typed character and paragraph character is same then
+            if (characters[charIndex].innerText === typeChar) {
+                characters[charIndex].classList.add("yes");
+            }
+            else {
+                mistake++;
+                characters[charIndex].classList.add("no");
+            }
+            // change the charecter index to verify next charecter
+            charIndex++;
+        }
+        // wpn setting and display it
+        let wpm = Math.round((((charIndex - mistake) / 5) / (maxTime - timeleft)) * 60);
+        // if the value of wpm is 0, infinity or not then set wpm = 0
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+        // print mistake value
+        mistakeTag.innerHTML = mistake;
+        wpmTag.innerHTML = wpm;
+    }
+    else {
+        textField.value = "";
+        clearInterval(timer);
+    }
+
+}
+function initTimer() {
+    if (timeleft > 0) {
+        timeleft--;
+        timeTag.innerHTML = timeleft;
+    }
+    else {
+        clearInterval(timer);
+    }
+}
+
+function resetAll() {
+    randomParagraph();
+    timeleft = maxTime;
+    charIndex = 0;
+    mistake = 0;
+    isTyping = 0;
+    mistakeTag.innerHTML = mistake;
+    wpmTag.innerHTML = 0;
+    timeTag.innerHTML = timeleft;
+    textField.value = "";
+    clearInterval(timer);
+}
+
+randomParagraph();
+textField.addEventListener('input', initField);
+reset.addEventListener('click', resetAll);
